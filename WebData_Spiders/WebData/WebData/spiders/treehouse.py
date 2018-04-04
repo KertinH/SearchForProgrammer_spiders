dab9d5c48e3866254235a6c6ee5eb1aa4f236ba4
KertinH <33600294+KertinH@users.noreply.github.com>
2018-04-05 01:00:06 +0800

    treehouse爬虫

---
+++ WebData_Spiders/WebData/WebData/spiders/treehouse.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+import datetime
+from ..scrapy_redis.spiders import RedisSpider
+from ..items import treehouseItem
+from ..Tools.md5 import get_md5
+
+
+class TreehouseSpider(RedisSpider):
+    name = 'TH'
+    allowed_domains = ['https://teamtreehouse.com/community']
+    # start_urls = ['https://teamtreehouse.com/community']
+    redis_key = 'treehouse:start_urls'
+
+    def parse(self, response):
+        if len(response.xpath("//ul[@class='discussion-list']/li[@class='discussion-item discussion-best-answer']").extract()) > 0:
+            data = response.xpath("//ul[@class='discussion-list']/li[@class='discussion-item discussion-best-answer']/div[@class='discussion-meta']")
+            for title,content,tag,create_time,url in zip(
+                    data.xpath("h2/a/text()").extract(),
+                    data.xpath("h2/a/text()").extract(),
+                    data.xpath("ul"),
+                    data.xpath("div/a/time/@datetime").extract(),
+                    data.xpath("h2/a/@href").extract()
+            ):
+                item = treehouseItem()
+                item['web_name'] = 'TreeHouse'
+                item['web_url'] = 'https://teamtreehouse.com/community'
+                item['title'] = title
+                item['content'] = content
+                item['tag'] = ','.join(tag.xpath("li/a/text()").extract())
+                try:
+                    item['create_time'] = datetime.datetime.strptime(re.match('.*?([\d-]+).*',create_time).group(1),'%Y-%m-%d').date()
+                except Exception as e:
+                    item['create_time'] = datetime.datetime.now().date()
+                item['url'] = response.urljoin(url)
+                item['url_md5'] = get_md5(item['url'])
+                yield item
+        if len(response.xpath("//div[@class='pagination-container']/a[contains(text(),'Next')]/@href").extract()) > 0:
+            next_page = response.xpath("//div[@class='pagination-container']/a[contains(text(),'Next')]/@href").extract_first()
+            next_url = response.urljoin(next_page)
+            yield scrapy.Request(next_url,callback=self.parse,dont_filter=True)
+        pass
